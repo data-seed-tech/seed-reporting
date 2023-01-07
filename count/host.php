df63823853cb5d776690c5dfd9f7ab19c4915bf4
IONESCU ADRIAN <adrian.ionescu@data-seed.tech>
2023-01-07 21:35:47 +0200

    host details added

--- count/host.php
+++ count/host.php
@@ -1,17 +1,39 @@
 <?php
 require_once("../connection.inc");
-//$ip = '114.119.154.82';
-//$ip = '93.115.248.45';  //simpliq
+require_once("../appCode.inc");
 
-//$ip = '66.249.66.198';     //crawl-66-249-66-198.googlebot.com
-//$ip = '114.119.145.198';
+/////////////////////////////////////
+//                PAGE              //
+/////////////////////////////////////
+$page =	@$_POST['p'];
+if($page == "")
+{
+    $page = @$_GET['p'];
+}
+if($page == "")
+{
+    $page = 1;
+}
+//print($page);
+
+
 //$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 $for =	@$_POST['hdnFor'];
 $msg = '';
+
+
 $ip  =	@$_POST['txtIP'];
+if($ip == "")
+    $ip  =	@$_POST['ip'];
 if($ip == "")
     $ip  =	@$_GET['ip'];
 //print($ip);
+
+
+
+
+
+
 
 if($ip != '')
 {
@@ -21,6 +43,10 @@ else
 {
     $hostname = "";
 }
+
+$ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip)); 
+$country = $ipdat->geoplugin_countryName; 
+$city = $ipdat->geoplugin_city; 
 
 
 //************************************************************************
@@ -38,18 +64,19 @@ if ($for == "SAVE")
 ?>
 <html>
 <head>
-<script language=javascript>
-function GetHost()
-{
-    document.frmForm.hdnFor.value = "GET_HOST";
-    document.frmForm.submit();
-}
-function Save()
-{
-    document.frmForm.hdnFor.value = "SAVE";
-    document.frmForm.submit();
-}
-</script>
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <script language=javascript>
+    function GetHost()
+    {
+        document.frmForm.hdnFor.value = "GET_HOST";
+        document.frmForm.submit();
+    }
+    function Save()
+    {
+        document.frmForm.hdnFor.value = "SAVE";
+        document.frmForm.submit();
+    }
+    </script>
 </head>
     
 <body>
@@ -72,6 +99,83 @@ function Save()
     <INPUT TYPE=button ID=btnGetHost ONCLICK='GetHost()' VALUE='&nbsp;&nbsp;&nbsp;GetHost&nbsp;&nbsp;&nbsp;' style="background-color:lightblue">&nbsp;&nbsp;&nbsp;
     <INPUT TYPE=button ID=btnSave ONCLICK='Save()' VALUE='&nbsp;&nbsp;&nbsp;Save&nbsp;&nbsp;&nbsp;' style="background-color:lightblue">&nbsp;&nbsp;&nbsp;
     <INPUT TYPE=hidden ID=hdnFor NAME=hdnFor>
-</form>
+</form><br/>
+
+
+Country: <?php print($country); ?><br/>
+City: <?php print($city); ?><br/><br/>
+<hr/>
+
+Navigation history:<br/>
+<?php
+require_once("../xml_functions.inc");
+
+
+$no_of_records_per_page = 15;
+$offset = ($page - 1) * $no_of_records_per_page;
+
+
+
+$total_pages_sql = "SELECT count(*) FROM counter WHERE counter.Ip = '" . $ip . "';";
+$result = mysqli_query($conn, $total_pages_sql);
+$total_rows = mysqli_fetch_array($result)[0];
+$total_pages = ceil($total_rows / $no_of_records_per_page);
+
+
+$query = "SELECT VisitTime, counter.ip, Page, Referrer, hosts.hostName
+        FROM counter
+        LEFT JOIN hosts ON counter.IP = hosts.IP
+        WHERE counter.Ip = '" . $ip . "'
+        ORDER BY VisitTime ASC
+        LIMIT ".$offset.", ".$no_of_records_per_page.";";
+
+PrintHTMLfromQuery($conn, $query);
+
+
+
+if($page > 1)
+{
+?>
+<a href='host.php?app=<?php print($appCode); ?>&amp;ip=<?php print($ip); ?>&p=1'>[ &lt;&lt; ]</a>
+&nbsp;
+<a href='host.php?app=<?php print($appCode); ?>&amp;ip=<?php print($ip); ?>&p=<?php print($page - 1); ?>'>[ &lt; ]</a>
+<?php
+}
+else
+{
+?>
+<font color="gray">[ &lt;&lt; ]</font>
+&nbsp;
+<font color="gray">[ &lt; ]</font>
+<?php
+}
+?>
+
+&nbsp;
+
+<font color="gray">&nbsp; [ Page <?php print($page); ?> ] &nbsp;</font>
+
+&nbsp;
+
+<?php
+if($page < $total_pages)
+{
+?>
+<a href='host.php?app=<?php print($appCode); ?>&amp;ip=<?php print($ip); ?>&p=<?php print($page + 1); ?>'>[ &gt; ]</a>
+&nbsp;
+<a href='host.php?app=<?php print($appCode); ?>&amp;ip=<?php print($ip); ?>&p=<?php print($total_pages); ?>'>[ &gt;&gt; ]</a>
+<?php
+}
+else
+{
+?>
+<font color="gray">[ &gt; ]</font>
+&nbsp;
+<font color="gray">[ &gt;&gt; ]</font>
+<?php
+}
+?>
+
+
 </body>
 </html>
